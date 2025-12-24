7a7ba3a54a4fb847d855fa1cffa5f89f29ce996e
your-github-username <your-email@example.com>
2025-12-24 08:59:05 +0530

    i added .env file in this api

--- db.js
+++ db.js
@@ -1,19 +1,21 @@
+require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const database = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "electricone",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 (async () => {
   try {
     const connection = await database.getConnection();
-    console.log("Database connection successful");
+    console.log("✅ Database connection successful");
     connection.release();
   } catch (error) {
-    console.error("Database connection failed:", error);
+    console.error("❌ Database connection failed:", error);
   }
 })();
 
