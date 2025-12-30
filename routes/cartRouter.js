192e4015a23f05fc31156a26ad2aa0d371ba2082
your-github-username <your-email@example.com>
2025-12-30 16:13:59 +0530

    i have complated to cart api

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -4,7 +4,7 @@ const upload=require("../middleware/multer");
 const {cartGet,cartAdd,cartUpdate,cartDelete}=require("../controllers/cartController");
 
 router.get("/cartsGet",cartGet);
-router.post("/cartPost",upload.single("image") ,cartAdd);
+router.post("/cartPost",cartAdd);
 router.put("/cartPut/:id",upload.single("image") ,cartUpdate);
 router.delete("/cartDelete/:id",upload.single("image") ,cartDelete);
 
