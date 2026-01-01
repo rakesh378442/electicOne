5b7f50703c3571c35283b0caab5b0702209d955b
your-github-username <your-email@example.com>
2026-01-01 18:52:45 +0530

    ok

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { register, login,getProfile } = require("../controllers/authcontrollers");
+const { register,login,getProfile} = require("../controllers/authcontrollers");
 
 router.post("/register", register);
 router.post("/login", login);
